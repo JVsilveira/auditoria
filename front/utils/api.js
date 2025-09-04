33dc3be2a0f5d95a97e1b879efc44c03fab4395e
JVsilveira <joaovitor.santossilveira@gmail.com>
2025-09-04 17:46:12 -0300

    remoção dos termos, criação dos requeriments

--- front/utils/api.js
+++ front/utils/api.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from "axios"
 
-export const uploadPdfs = async (files) => {
-  const formData = new FormData();
+export const uploadPdfs = async files => {
+  const formData = new FormData()
   for (let i = 0; i < files.length; i++) {
-    formData.append("pdfs", files[i]);
+    formData.append("pdfs", files[i])
   }
 
   const response = await axios.post("http://localhost:8000/upload", formData, {
     headers: { "Content-Type": "multipart/form-data" },
-  });
-  return response.data;
-};
+  })
+  return response.data
+}
