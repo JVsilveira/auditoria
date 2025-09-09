db71280439f8817855cbf6a21c83f8cb6769cdae
JVsilveira <joaovitor.santossilveira@gmail.com>
2025-09-09 15:16:26 -0300

    ADICIONADO VERIFICAÇÃO DO TIPO DE ATIVO E VERIFICAÇÃO DA MARCA DO ATIIVO

--- back/utils/validar_termo.py
+++ back/utils/validar_termo.py
@@ -2,13 +2,22 @@ import re
 from utils.validar_hostname import validar_hostname   
 
 def validar_termo(dados: dict) -> bool:
+
     # --- Lista de modelos que dispensam validação de NF ---
     modelos_excecao_nf = [
         "OPTIPLEX 7070", "LATITUDE 7400", "LATITUDE 5400", "LATITUDE 5420",
         "7070", "7400", "5400", "5420"
     ]
 
+    # --- Tipos de ativo ---
+    tipos_validos = ["DESKTOP", "MINIDESK", "MINIDESKTOP", "NOTEBOOK"]
+
+    # --- Marcas ---
+    marcas_validas = ["DELL", "LENOVO", "HP", "ACER", "ASUS", "SAMSUNG", "POSITIVO", "APPLE"]
+
     modelo = str(dados.get("MODELO", "")).strip().upper()
+    tipo = str(dados.get("TIPO", "")).strip().upper()
+    marca = str(dados.get("MARCA", "")).strip().upper()
     
     # --- Valida NF ---
     nf = str(dados.get("NF", "")).strip()
@@ -28,6 +37,16 @@ def validar_termo(dados: dict) -> bool:
         if not serial_monitor or re.fullmatch(r"-+", serial_monitor):
             return False
 
+    # --- Verifica tipo ---
+    if dados.get("TERMO") == "CONCESSÃO" or dados.get("TERMO") == "DEVOLUÇÃO":
+        if  tipo not in tipos_validos:
+            return False
+
+    # --- Verifica marca ---
+    if dados.get("TERMO") == "CONCESSÃO" or dados.get("TERMO") == "DEVOLUÇÃO":
+        if marca not in marcas_validas:
+            return False
+
     # --- Verifica hostname ---
     if dados.get("TERMO") == "CONCESSÃO":
         hostname = str(dados.get("HOSTNAME", "")).strip()
@@ -35,7 +54,7 @@ def validar_termo(dados: dict) -> bool:
         if not validar_hostname(hostname, matricula if matricula else None):
             return False
 
-    # --- Verifica chamado somente para concessão ---
+    # --- Verifica chamados ---
     if dados.get("TERMO") == "CONCESSÃO":
         chamado = str(dados.get("CHAMADO", "")).strip().upper()
 
